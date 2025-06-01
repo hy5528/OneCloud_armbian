27ddaab61fbf031b875875fe447a2b769677fabe
hy5528 <95360525+hy5528@users.noreply.github.com>
2025-06-01 20:50:08 +0800

    Add files via upload

---
+++ patch/userpatches/customize-image.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# 备份原有的sources.list文件
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+ 
+# 更新sources.list文件
+sudo tee /etc/apt/sources.list <<EOF
+deb https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib 
+deb-src https://mirrors.aliyun.com/debian/ bookworm main non-free non-free-firmware contrib 
+deb https://mirrors.aliyun.com/debian-security/ bookworm-security main 
+deb-src https://mirrors.aliyun.com/debian-security/ bookworm-security main 
+deb https://mirrors.aliyun.com/debian/ bookworm-updates main non-free non-free-firmware contrib 
+deb-src https://mirrors.aliyun.com/debian/ bookworm-updates main non-free non-free-firmware contrib 
+deb https://mirrors.aliyun.com/debian/ bookworm-backports main non-free non-free-firmware contrib 
+deb-src https://mirrors.aliyun.com/debian/ bookworm-backports main non-free non-free-firmware contrib
+EOF
+# 更新软件包
+sudo apt-get update
+sudo apt dist-upgrade -y
+
+# 安装 Docker
+sudo apt-get install -y ca-certificates curl gnupg lsb-release alsa-utils 
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo systemctl start docker
+
+# 清理缓存
+apt autoremove -y && apt autoclean && apt remove -y && apt clean
+
